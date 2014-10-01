1beec92957506618dcd8d47b3948697a57aa7c1f
Shy Ward <shyward@Shys-MacBook-Pro.local>
2014-10-01 10:45:22 -0400

    Initial Project

---
+++ floor_creator/floor_creator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  floor_creator
+//
+//  Created by Shy Ward on 10/1/14.
+//  Copyright (c) 2014 Shy Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
